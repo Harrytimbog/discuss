c963363d3df3312730aa9d0598b1890943d00b23
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-24 16:52:09 +0000

    Wrapping Auth in Server Actions

---
+++ src/actions/index.ts
@@ -0,0 +1,13 @@
+// Totally Optional but Recommended
+
+"use server";
+
+import * as auth from "@/auth";
+
+export async function signIn() {
+  return auth.signIn("github");
+}
+
+export async function signOut() {
+  return auth.signOut();
+}
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+// export async function GET() {}
+
+// export async function POST() {}
+
+export { GET, POST } from "@/auth";
