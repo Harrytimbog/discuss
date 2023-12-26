43db369a7e0e1e75fac71edefb925b67e3d87318
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-26 22:22:50 +0000

    Adding Validation Errors with Zod and using UseFormState to capture this form functionality

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,42 @@
 "use server";
+import { z } from "zod";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   // console.log(name, description);
-  
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    // console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  return {
+    errors: {},
+  };
+
   // TODO: Revalidate the homepage
 }
