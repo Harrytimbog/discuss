bbc8e9b8411348dea38219c125e4c4e48d42f808
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-28 23:15:04 +0000

    Deduplicating Requests with Cache

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,23 +1,24 @@
 import type { Comment } from "@prisma/client";
 import { db } from "@/db";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export async function fetchCommentsByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("Making A query");
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("Making A query");
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
