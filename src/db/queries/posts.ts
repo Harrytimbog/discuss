5b8bccbed0ff87a063c944ba4f400e2f34d80614
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-28 17:25:26 +0000

    Alternative Type Names and Query Definitions

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,11 +1,17 @@
 import type { Post } from "@prisma/client";
 import { db } from "@/db";
 
+// Find a better name instead of PostWithData. e.g PostForListDisplay, Enriched PostList etc
 export type PostWithData = Post & {
   topic: { slug: string };
   user: { name: string | null };
   _count: { comments: number };
 };
+
+// Alternative / Shortcut if writing the typescript manually is confusing me. I will just remove the default return type on the function below
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
 
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
@@ -17,5 +23,3 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
     },
   });
 }
-
-
