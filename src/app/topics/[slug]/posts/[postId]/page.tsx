21970152ee9606abcb633da11083c92830d94211
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-28 22:34:35 +0000

    Fetching the BigList

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 
@@ -21,7 +22,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       </Link>
       <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
-      {/* <CommentList comments={comments} /> */}
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
     </div>
   );
 }
