73b16e4ffbc5abaf808eed5d7859e8baf10fba73
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-29 23:17:32 +0000

    >

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  output: "standalone",
-  webpack: (config, { isServer }) => {
-    config.resolve.alias["~"] = path.join(__dirname, "src");
-    return config;
-  },
 };
 
 module.exports = nextConfig;
