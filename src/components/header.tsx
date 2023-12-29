7ec94a1ea56d56b0b57c1f9b11fcd8608436571c
Timilehin Arigbede <harriemannie@gmail.com>
2023-12-29 23:05:27 +0000

    Attemp to fix module not found issue for header.tsx in layout on deploying to vercel

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+} from "@nextui-org/react";
+import HeaderAuth from "./header-auth";
+import SearchInput from "./search-input";
+import { Suspense } from "react";
+
+export default function Header() {
+  return (
+    <Navbar className="shadow mt-1 mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bond">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Suspense>
+            <SearchInput />
+          </Suspense>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
+}
